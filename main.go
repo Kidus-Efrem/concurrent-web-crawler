892f4a0dbc491d5912db8861d630fb30fe3b0ec4
kidus <kidusefrem2@gmail.com>
2026-05-28 02:19:06 +0300

    bfs crawler, visited tracking , queue system

--- main.go
+++ main.go
@@ -3,38 +3,63 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"golang.org/x/net/html"
 )
 
 func main() {
-	url := "https://example.com"
+	startURL := "https://example.com"
 
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	queue := []string{startURL}
+	visited := make(map[string]bool)
+
+	for len(queue) > 0 {
+		// pop first element (BFS)
+		url := queue[0]
+		queue = queue[1:]
+
+		if visited[url] {
+			continue
+		}
+		visited[url] = true
+
+		fmt.Println("Visiting:", url)
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		doc, err := html.Parse(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Println("Parse error:", err)
+			continue
+		}
+
+		links := extractLinks(doc)
+
+		for _, link := range links {
+			queue = append(queue, link)
+		}
 	}
-	defer resp.Body.Close()
-
-	doc, err := html.Parse(resp.Body)
-	links := extractLinks(doc)
-
-	for _, link := range links{
-		fmt.Println((link))
-	}
-
 }
-
-func extractLinks(n *html.Node)[]string{
+func extractLinks(n *html.Node) []string {
 	var links []string
-	if n.Type == html.ElementNode && n.Data == "a"{
-		for _, attr := range n.Attr{
-			if attr.Key=="href"{
+
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, attr := range n.Attr {
+			if attr.Key == "href" {
 				links = append(links, attr.Val)
-			}		}
+			}
+		}
 	}
-	for child := n.FirstChild; child !=nil; child = child.NextSibling{
-		links = append(links, extractLinks(child)...)
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = append(links, extractLinks(c)...)
 	}
+
 	return links
 }
